09764cd2c464ce043fa355dea9d3efe42c83013a
kitdim <kitaykin9808@gmail.com>
2022-09-09 19:34:22 +0300

    Добавил страницы для контента

---
+++ LauncherKit/View/SC.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LauncherKit.View
+{
+    /// <summary>
+    /// Interaction logic for SC.xaml
+    /// </summary>
+    public partial class SC : Page
+    {
+        public SC()
+        {
+            InitializeComponent();
+        }
+    }
+}
